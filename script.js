b40de4dc129163b612709ecbf1138d4dc65b45c1
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-03 20:56:55 +0600

    why reducers are called reducers

--- script.js
+++ script.js
@@ -1,74 +1,29 @@
-//apply dom elements
-const counterEl = document.getElementById("counter");
-const incrementEl = document.getElementById("increment");
-const decrementEl = document.getElementById("decrement");
+const actions = [
+  { type: "increment", payload: 1 },
+  { type: "increment", payload: 1 },
+  { type: "increment", payload: 1 },
+  { type: "decrement", payload: 1 },
+];
 
 const initialState = {
   value: 0,
-  properties: {
-    a: 5,
-    b: 6,
-  },
 };
 
-//action identifiers
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
-
-//action creators
-const increment = (value) => {
-  return {
-    type: INCREMENT,
-    payload: value,
-  };
-};
-
-const decrement = (value) => {
-  return {
-    type: DECREMENT,
-    payload: value,
-  };
-};
-
-function counterReducer(state = initialState, action) {
-  if (action.type === INCREMENT) {
+const countReducer = (state, action) => {
+  if (action.type === "increment") {
     return {
       ...state,
       value: state.value + action.payload,
     };
-  } else if (action.type === DECREMENT) {
+  } else if (action.type === "decrement") {
     return {
       ...state,
       value: state.value - action.payload,
     };
-  } else if (action.type === ITEST) {
-    return {
-      ...state,
-      properties: {
-        ...state.properties,
-        b: state.properties + 1,
-      },
-    };
   } else {
     return state;
   }
-}
-
-const store = Redux.createStore(counterReducer);
-
-const render = () => {
-  const state = store.getState();
-  counterEl.innerText = state.value.toString();
 };
 
-render();
-
-store.subscribe(render);
-
-incrementEl.addEventListener("click", () => {
-  store.dispatch(increment(5));
-});
-
-decrementEl.addEventListener("click", () => {
-  store.dispatch(decrement(2));
-});
+const finalResult = actions.reduce(countReducer, initialState);
+console.log(finalResult);
