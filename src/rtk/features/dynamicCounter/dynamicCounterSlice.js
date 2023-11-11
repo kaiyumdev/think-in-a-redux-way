481fd4cf5403023995f5822642a094dfc5476611
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-11 13:10:52 +0600

    implementing extra reducers successfully

--- src/rtk/features/dynamicCounter/dynamicCounterSlice.js
+++ src/rtk/features/dynamicCounter/dynamicCounterSlice.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-undef */
+
+const { counterActions } = require("../counter/counterSlice");
+
 /* eslint-disable no-unused-vars */
 const createSlice = require("@reduxjs/toolkit").createSlice;
 
@@ -12,11 +15,22 @@ const dynamicCounterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state, action) => {
-      state.counter += action.payload;
+      state.count += action.payload;
     },
     decrement: (state, action) => {
-      state.counter -= action.payload;
+      state.count -= action.payload;
     },
+  },
+  //   extraReducers: {
+  //     // ["counter/increment"]: (state, action) => {
+  //     //   state.count += 1;
+  //     // },
+
+  //   },
+  extraReducers: (builder) => {
+    builder.addCase(counterActions.increment, (state, action) => {
+      state.count += 1;
+    });
   },
 });
 
