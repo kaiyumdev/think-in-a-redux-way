f90b07f4b12f135a69d3e5553d1c0655f6d63f15
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-11 09:47:12 +0600

    middleware concept -redux-logger

--- src/rtk/app/store.js
+++ src/rtk/app/store.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-undef */
 const configureStore = require("@reduxjs/toolkit").configureStore;
+const { createLogger } = require("redux-logger");
+// const { getDefaultMiddleware } = require("@reduxjs/toolkit");
 const counterReducer = require("../features/counter/counterSlice.js");
 const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounterSlice.js");
 
+const logger = createLogger();
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     dynamicCounter: dynamicCounterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
