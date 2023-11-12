d269b7c07eb128707caa5c30979b59c24f353314
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-12 19:14:55 +0600

    create store by redux-thunk successfully

--- src/redux/asyncAction.js
+++ src/redux/asyncAction.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-undef */
+import { applyMiddleware, createStore } from "redux";
 /* eslint-disable no-unused-vars */
 //initialState
+const fetch = require("node-fetch");
 const initialState = {
   loading: false,
   posts: [],
@@ -21,7 +24,56 @@ const fetchPostsSucceeded = (posts) => {
 
 const fetchPostsFailed = (error) => {
   return {
-    type: "posts/succeeded",
+    type: "posts/failed",
     payload: error,
   };
 };
+
+//reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "posts/requested":
+      return {
+        ...state,
+        leading: true,
+      };
+
+    case "posts/succeeded":
+      return {
+        ...state,
+        leading: false,
+        error: "",
+        posts: action.payload,
+      };
+
+    case "posts/failed":
+      return {
+        ...state,
+        leading: false,
+        error: action.payload.message,
+        posts: [],
+      };
+
+    default:
+      break;
+  }
+};
+
+//thunk function
+const fetchPosts = () => {
+  return async (dispatch) => {
+    dispatch(fetchPostsRequested());
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?_limit=5"
+      );
+      const posts = response.json();
+      dispatch(fetchPostsSucceeded(posts));
+    } catch (error) {
+      dispatch(fetchPostsFailed(error));
+    }
+  };
+};
+
+//create store
+const store = createStore(reducer, applyMiddleware());
