05cad40f4dd193f20ea2b49fc62ac4257b27a89f
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-12 20:36:03 +0600

    implementing redux/index and store successfully

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,26 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import { applyMiddleware, createStore } from "redux";
-import rootReducer from "./rootReducer";
-import myLogger from "./middlewares/myLogger";
-import logger from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { applyMiddleware, createStore } from "redux";
+// import rootReducer from "./rootReducer";
+// import myLogger from "./middlewares/myLogger";
+// import logger from "redux-logger";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(myLogger, logger))
-);
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(myLogger, logger))
+// );
 
-export default store;
+// export default store;
+// const { createStore, combineReducers } = require("redux");
+// const counterReducer = require("./counter/reducer");
+// const dynamicCounterReducer = require("./dynamicCounter/reducer");
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  dynamicCounter: dynamicCounterReducer,
+});
+
+const store = createStore(rootReducer);
+
+module.exports = store;
