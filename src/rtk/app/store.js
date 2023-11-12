089270215bb800057367ec4fb26ab01f69b5fce7
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-12 21:21:15 +0600

    implements asyncThunk by redux-toolkit successfully

--- src/rtk/app/store.js
+++ src/rtk/app/store.js
@@ -4,12 +4,14 @@ const { createLogger } = require("redux-logger");
 // const { getDefaultMiddleware } = require("@reduxjs/toolkit");
 const counterReducer = require("../features/counter/counterSlice.js");
 const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounterSlice.js");
+const postReducer = require("../features/post/postSlice.js");
 
 const logger = createLogger();
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     dynamicCounter: dynamicCounterReducer,
+    post: postReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
--- src/rtk/index.js
+++ src/rtk/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
 const store = require("./app/store");
 // const { counterActions } = require("./features/counter/counterSlice");
-const { dynamicCounterActions } = require("./features/counter/counterSlice");
+// const { dynamicCounterActions } = require("./features/counter/counterSlice");
+const { fetchPosts } = require("./features/post/postSlice");
 
 //initialState
 console.log(store.getState());
@@ -18,7 +19,8 @@ store.subscribe(() => {
 // store.dispatch(counterActions.decrement());
 
 //dispatch actions
-store.dispatch(dynamicCounterActions.increment(3));
-store.dispatch(dynamicCounterActions.increment(4));
+// store.dispatch(dynamicCounterActions.increment(3));
+// store.dispatch(dynamicCounterActions.increment(4));
 
-store.dispatch(dynamicCounterActions.decrement(2));
+// store.dispatch(dynamicCounterActions.decrement(2));
+store.dispatch(fetchPosts());
